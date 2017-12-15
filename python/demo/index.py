82df294266ebceb85c474b88edbebe2ebd744a70
xiaobao <1003859826@qq.com>
2017-12-15 09:00:57 +0800

    add python

---
+++ python/demo/index.py
@@ -0,0 +1,21 @@
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def',
+# 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import',
+# 'in', 'is', 'lambda','nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+import json,time,calendar
+
+#d=dict([("name",1),("code","21")])
+#print(json.dumps(d))
+
+#print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()))
+
+obj={"name":"name1","code":"code1"}
+obj=time
+print(type(obj))
+
+
+
+
+
+
+
+
