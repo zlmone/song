164e55c7dfa6dff6d46b3865dd20f4ce34ea9801
xiaobao <1003859826@qq.com>
2017-12-18 14:57:21 +0800

    update python

--- python/demo/index.py
+++ python/demo/index.py
@@ -1,16 +1,21 @@
 # ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def',
 # 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import',
 # 'in', 'is', 'lambda','nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
-import json,time,calendar
 
-#d=dict([("name",1),("code","21")])
-#print(json.dumps(d))
+import requests
+from bs4 import BeautifulSoup
+from util import lib
 
-#print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()))
+url="http://bbs.my9200.com/a/zp/index.html"
+response=requests.get(url)
+response.encoding="utf-8"
+html=response.text
 
-obj={"name":"name1","code":"code1"}
-obj=time
-print(type(obj))
+bf=BeautifulSoup(html,"html.parser")
+title=bf.select("title")
+titleText=title[0].text
+print(titleText)
+
 
 
 
