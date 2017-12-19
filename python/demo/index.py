3806adbc3f12ba677e1c517748256dd0c6e8a2eb
xiaobao <1003859826@qq.com>
2017-12-19 18:39:02 +0800

    update python.index.py

--- python/demo/index.py
+++ python/demo/index.py
@@ -1,26 +1,45 @@
-# ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def',
-# 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import',
-# 'in', 'is', 'lambda','nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+#! /usr/bin/python
+# coding = utf-8
+import re
 
-import requests
-from bs4 import BeautifulSoup
-from util import lib
+"""
+字符串测试
+"""
+s="wangso" \
+  "nghua"
+print("{:.2f}".format(21.9562211212))
+print("{:0>5d}".format(21))
+print(s.zfill(20))
+print(s.rjust(20,"0"))
+print(s.partition("g"))
+print(s.rpartition("g"))
+#isalnum：数字或字母
+#isalpha：字母
+#isdigit：纯数字，不包含负数和小数
 
-url="http://bbs.my9200.com/a/zp/index.html"
-response=requests.get(url)
-response.encoding="utf-8"
-html=response.text
+"""
+"""
+arr=["山景城","常春藤","祈福二手","亚运城二手","尚城复式","里享家","凤凰城二手"]
+tup=tuple(arr)
+tup1=tuple(arr)
 
-bf=BeautifulSoup(html,"html.parser")
-title=bf.select("title")
-titleText=title[0].text
-print(titleText)
-
+numstr="223ess21"
+print(int(re.sub("\D","",numstr)))
 
 
-
-
-
-
-
-
+"""
+int(x [,base ])         将x转换为一个整数    
+long(x [,base ])        将x转换为一个长整数    
+float(x )               将x转换到一个浮点数    
+complex(real [,imag ])  创建一个复数    
+str(x )                 将对象 x 转换为字符串    
+repr(x )                将对象 x 转换为表达式字符串    
+eval(str )              用来计算在字符串中的有效Python表达式,并返回一个对象    
+tuple(s )               将序列 s 转换为一个元组    
+list(s )                将序列 s 转换为一个列表    
+chr(x )                 将一个整数转换为一个字符    
+unichr(x )              将一个整数转换为Unicode字符    
+ord(x )                 将一个字符转换为它的整数值    
+hex(x )                 将一个整数转换为一个十六进制字符串    
+oct(x )                 将一个整数转换为一个八进制字符串   
+"""
