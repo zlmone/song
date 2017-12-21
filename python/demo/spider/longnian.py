38e880a0926d27ca1a9781c077b74cc9b1241ce3
song <1003859826@qq.com>
2017-12-21 21:01:41 +0800

    update python

--- python/demo/spider/longnian.py
+++ python/demo/spider/longnian.py
@@ -5,30 +5,34 @@ import song.config as config
 import song.http.jsoup as jsoup
 from song.helper import string
 
-now = "2017-12-20"
+def run(now):
+    domain = "http://bbs.my9200.com"
+    dateFmt = config.fmt.date
+    url = domain + "/a/zp/index.html"
+    doc = jsoup.load(url)
+    detailUrls = []
+    for item in doc.select("div.mnewest li"):
+        link = item.select("span.stitle a")[0]
+        href = link.get("href")
+        date = item.select("span.wp_time")[0].text
+        if string.isempty(now):
+            now = time.strftime(dateFmt)
+        beginDate = time.mktime(time.strptime(now, dateFmt))
+        responseDate = time.mktime(time.strptime(date, dateFmt))
+        if (responseDate >= beginDate):
+            detailUrls.append(domain + href)
 
-domain = "http://bbs.my9200.com"
-dateFmt = config.fmt.date
-url = domain + "/a/zp/index.html"
-doc = jsoup.load(url)
-detailUrls = []
+    for detailUrl in detailUrls:
+        detailDoc = jsoup.load(detailUrl)
+        pageWrap = detailDoc.select("div.pagecon")[0].text
+        downloadUrl = pageWrap.split("：")[1]
+        title=detailDoc.select("font[color='#cacaca']")[0].text
+        if not string.isempty(downloadUrl):
+            print(downloadUrl)
+            print(title)
 
-for item in doc.select("div.mnewest li"):
-    link = item.select("span.stitle a")[0]
-    href = link.get("href")
-    date = item.select("span.wp_time")[0].text
-    if string.isempty(now):
-        now = time.strftime(dateFmt)
-    beginDate = time.mktime(time.strptime(now, dateFmt))
-    responseDate = time.mktime(time.strptime(date, dateFmt))
-    if (responseDate >= beginDate):
-        detailUrls.append(domain + href)
+if __name__ == "__main__":
+    now = "2017-12-20"
+    run(now)
 
-for detailUrl in detailUrls:
-    detailDoc = jsoup.load(detailUrl)
-    pageWrap = detailDoc.select("div.pagecon")[0].text
-    downloadUrl = pageWrap.split("：")[1]
-    title=detailDoc.select("font[color='#cacaca']")[0].text
-    if not string.isempty(downloadUrl):
-        print(downloadUrl)
-        print(title)
+
