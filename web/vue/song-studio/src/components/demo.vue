ecdf4fe5e9c03b4303b3359fc49a5cc9ebd74f59
songhuaxiaobao <1003859826@qq.com>
2018-03-22 23:30:12 +0800

    add vue

---
+++ web/vue/song-studio/src/components/demo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <el-button type="primary" icon="el-icon-search">查询</el-button>
+    <el-progress :percentage="0"></el-progress>
+    <el-progress :percentage="70"></el-progress>
+    <el-progress :percentage="100" status="success"></el-progress>
+    <el-progress :percentage="50" status="exception"></el-progress>
+  </div>
+</template>
