8a86e749f710447300a8bf4e14ec27a4ccc8d6b0
songhuaxiaobao <1003859826@qq.com>
2018-04-14 02:45:01 +0800

    add

---
+++ java/song-api/song-api-user/src/main/java/song/api/user/SongApiUserApplication.java
@@ -0,0 +1,12 @@
+package song.api.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongApiUserApplication.class, args);
+	}
+}
--- java/song-api/song-api-user/src/main/java/song/api/user/service/impl/DemoService.java
+++ java/song-api/song-api-user/src/main/java/song/api/user/service/impl/DemoService.java
@@ -1,5 +1,6 @@
 package song.api.user.service.impl;
 
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import song.api.user.base.BaseService;
@@ -15,6 +16,7 @@ public class DemoService extends BaseService implements IDemoService {
     private IDemoDao demoDao;
 
     public List<Demo> getByName(String name) {
+        PageHelper.startPage(1, 2);
         return demoDao.getByName(name);
     }
 }
