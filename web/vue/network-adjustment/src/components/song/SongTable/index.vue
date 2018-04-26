7c389890d537793cd59905b4ef177389e570081e
songhuaxiaobao <1003859826@qq.com>
2018-04-26 17:53:34 +0800

    add

--- web/vue/network-adjustment/src/components/song/SongTable/index.vue
+++ web/vue/network-adjustment/src/components/song/SongTable/index.vue
@@ -3,28 +3,10 @@
     <el-table :data="rows" v-loading.body="loading" 
       element-loading-text="正在努力加载数据..." 
       border fit highlight-current-row stripe >
-      <el-table-column v-for="(item,key) in columns" 
-        v-if="item.isDataColumn!==false"
-        :key="key"
-        :prop="item.prop"
-        :label="item.label"
-        :width="item.width"
-        :align="item.align"
-        :fixed="item.fixed"
-        :sortable="item.sortable"
-        :resizable="item.resizable"
-        ></el-table-column>
-      <el-table-column v-else
-        :key="key"
-        :prop="item.prop"
-        :label="item.label"
-        :width="item.width"
-        :align="item.align"
-        :fixed="item.fixed"
-        :sortable="item.sortable"
-        :resizable="item.resizable">
+      <el-table-column v-for="(item,key) in columns" v-bind="item">
           <template scope="scope">
-              <slot :name="item.prop" :$index="scope.$index" :row="scope.row"></slot>
+              <slot v-if="item.slot" :name="item.prop" :$index="scope.$index" :row="scope.row"></slot>
+              <span v-else>{{scope.row[item.prop]}}</span>
           </template>
       </el-table-column>
     </el-table>
@@ -34,7 +16,6 @@
 
 <script>
 import SongPager from '@/components/song/SongPager'
-import Vue from 'vue'
 import request from '@/utils/request'
 
 export default {
@@ -47,38 +28,38 @@ export default {
       return []
     }},
     url:{type:String},
-    method:{type:String,default:"GET"}
+    method:{type:String,default:"GET"},
+    params:{type:Object,default:function(){return {}}}
   },
   data() {
     return {
-      queryParams:{},
       rows: null,
       total:0,
       loading: true
     }
   },
   created() {
-    this.load({id:"12"})
+    this.load()
   },
   methods: {
     load:function(loadParams){
         this.loadPage(loadParams)
     },
     reload:function(loadParams){
-        this.queryParams["pageNum"]=1;
+        this.params["pageNum"]=1;
         this.loadPage(loadParams)
     },
     loadPage(pageParams) {
       this.loading = true
       if(pageParams){
         for (var key in pageParams) {
-          this.queryParams[key] = pageParams[key];
+          this.params[key] = pageParams[key];
         }
       }
       request({
         url: this.url,
-        method:"get",
-        params:this.queryParams
+        method:this.method,
+        params:this.params
       }).then(response => {
         this.rows = response.data.rows
         this.total=response.data.total
