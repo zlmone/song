066cdd1c59138cb7de7ebd8fb7298c730673f1ab
songhuaxiaobao <1003859826@qq.com>
2018-05-30 22:35:04 +0800

    add

--- java/song-api/song-api-common/src/main/java/song/api/common/config/JWTAudienceConfig.java
+++ java/song-api/song-api-common/src/main/java/song/api/common/config/JWTAudienceConfig.java
@@ -1,9 +1,22 @@
 package song.api.common.config;
 
+import song.common.config.Configuration;
 import song.common.toolkit.security.jwt.JWTAudience;
 
+import java.io.IOException;
+
 public class JWTAudienceConfig {
-    public static JWTAudience getAudience() {
-        return new JWTAudience();
+    private static JWTAudience audience;
+    public static JWTAudience getAudience() throws IOException {
+        if(audience==null)
+        {
+            Configuration config = new Configuration("jwt.properties");
+            audience = new JWTAudience();
+            audience.setAudience(config.getProperty("jwt.audience"));
+            audience.setSecret(config.getProperty("jwt.secret"));
+            audience.setIssuer(config.getProperty("jwt.issuer"));
+            audience.setExpireMin(config.getPropertyInt("jwt.expireMin"));
+        }
+        return audience;
     }
 }
