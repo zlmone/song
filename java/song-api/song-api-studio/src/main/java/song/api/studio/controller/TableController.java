5babcb49fb5142f4f7bae72675291946571677e7
songhuaxiaobao <1003859826@qq.com>
2018-06-04 00:31:47 +0800

    add

--- java/song-api/song-api-studio/src/main/java/song/api/studio/controller/TableController.java
+++ java/song-api/song-api-studio/src/main/java/song/api/studio/controller/TableController.java
@@ -2,24 +2,20 @@ package song.api.studio.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import song.api.studio.model.Table;
 import song.api.studio.service.ITableService;
 import song.common.toolkit.base.BaseController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/table")
 public class TableController extends BaseController {
     @Autowired
     private ITableService tableService;
 
-    @RequestMapping(value = "/getlist", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping(value = "/list")
     public List<Table> getList(String projectId) {
         return tableService.getList(projectId);
     }
--- java/song-api/song-api-studio/src/main/java/song/api/studio/controller/TemplateController.java
+++ java/song-api/song-api-studio/src/main/java/song/api/studio/controller/TemplateController.java
@@ -3,23 +3,20 @@ package song.api.studio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import song.api.studio.model.Template;
 import song.api.studio.service.ITemplateService;
 import song.common.toolkit.base.BaseController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/template")
 public class TemplateController extends BaseController {
     @Autowired
     private ITemplateService templateService;
 
-    @RequestMapping(value = "/getlist", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping(value = "/list")
     public List<Template> getList() {
         return null;
     }
