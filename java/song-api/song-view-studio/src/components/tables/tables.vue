19199d8ef527bb05b88a7152ed4bd40593f906b1
songhuaxiaobao <1003859826@qq.com>
2019-03-13 23:19:09 +0800

    add

--- java/song-api/song-view-studio/src/components/tables/tables.vue
+++ java/song-api/song-view-studio/src/components/tables/tables.vue
@@ -37,15 +37,10 @@
       <slot name="header" slot="header"></slot>
       <slot name="footer" slot="footer"></slot>
       <slot name="loading" slot="loading"></slot>
+
+      <slot name="body"></slot>
+
     </Table>
-    <div v-if="searchable && searchPlace === 'bottom'" class="search-con search-con-top">
-      <Select v-model="searchKey" class="search-col">
-        <Option v-for="item in columns" v-if="item.key !== 'handle'" :value="item.key" :key="`search-col-${item.key}`">{{ item.title }}</Option>
-      </Select>
-      <Input placeholder="输入关键字搜索" class="search-input" v-model="searchValue"/>
-      <Button class="search-btn" type="primary"><Icon type="search"/>&nbsp;&nbsp;搜索</Button>
-    </div>
-    <a id="hrefToExportTable" style="display: none;width: 0px;height: 0px;"></a>
   </div>
 </template>
 
