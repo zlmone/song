28edbd7ce5864dc2760028acb13bcbf5411a1d47
songhuaxiaobao <1003859826@qq.com>
2019-03-28 17:47:01 +0800

    4额

--- java/song-api/song-view-studio/src/components/main/main.vue
+++ java/song-api/song-view-studio/src/components/main/main.vue
@@ -1,21 +1,14 @@
 <template>
   <Layout style="height: 100%" class="main">
-    <Sider hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed" class="left-sider" :style="{overflow: 'hidden'}">
-      <side-menu accordion ref="sideMenu" :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
-        <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
-        <div class="logo-con">
-          <img v-show="!collapsed" :src="maxLogo" key="max-logo" />
-          <img v-show="collapsed" :src="minLogo" key="min-logo" />
-        </div>
-      </side-menu>
-    </Sider>
-    <Layout>
+ 
+ 
       <Header class="header-con">
-        <header-bar :collapsed="collapsed" @on-coll-change="handleCollapsedChange">
+        <header-bar  @on-coll-change="handleCollapsedChange">
           <user :message-unread-count="unreadCount" :user-avatar="userAvatar"/>
           <language v-if="$config.useI18n" @on-lang-change="setLocal" style="margin-right: 10px;" :lang="local"/>
-          <error-store v-if="$config.plugin['error-store'] && $config.plugin['error-store'].showInHeader" :has-read="hasReadErrorPage" :count="errorCount"></error-store>
           <fullscreen v-model="isFullscreen" style="margin-right: 10px;"/>
+
+          
         </header-bar>
       </Header>
       <Content class="main-content-con">
@@ -31,7 +24,7 @@
           </Content>
         </Layout>
       </Content>
-    </Layout>
+ 
   </Layout>
 </template>
 <script>
@@ -96,7 +89,7 @@ export default {
       return this.$store.state.app.hasReadErrorPage
     },
     unreadCount () {
-      return this.$store.state.user.unreadCount
+      return 0
     }
   },
   methods: {
--- java/song-api/song-view-studio/src/locale/index.js
+++ java/song-api/song-view-studio/src/locale/index.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import { localRead } from '@/libs/util'
 import customZhCn from './lang/zh-CN'
-import customZhTw from './lang/zh-TW'
+ 
 import customEnUs from './lang/en-US'
 import zhCnLocale from 'iview/src/locale/lang/zh-CN'
 import enUsLocale from 'iview/src/locale/lang/en-US'
-import zhTwLocale from 'iview/src/locale/lang/zh-TW'
+ 
 
 Vue.use(VueI18n)
 
@@ -21,7 +21,7 @@ Vue.config.lang = lang
 Vue.locale = () => {}
 const messages = {
   'zh-CN': Object.assign(zhCnLocale, customZhCn),
-  'zh-TW': Object.assign(zhTwLocale, customZhTw),
+ 
   'en-US': Object.assign(enUsLocale, customEnUs)
 }
 const i18n = new VueI18n({
