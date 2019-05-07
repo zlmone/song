4a220854f17c5d5235602881f9b73960ceeba2c0
songhuaxiaobao <1003859826@qq.com>
2019-05-07 18:49:19 +0800

    add

--- java/song-api/song-api-studio/src/main/java/song/api/studio/controller/ConnectionController.java
+++ java/song-api/song-api-studio/src/main/java/song/api/studio/controller/ConnectionController.java
@@ -1,8 +1,9 @@
 package song.api.studio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import song.api.studio.model.Connection;
 import song.api.studio.service.IConnectionService;
 import song.common.toolkit.base.BaseController;
