f3d9cccd64fab165365c41b8df7e24f7fe38b21d
songhuaxiaobao <1003859826@qq.com>
2019-05-09 23:09:42 +0800

    add

--- java/song-api/song-api-studio/src/main/java/song/api/studio/controller/ConnectionController.java
+++ java/song-api/song-api-studio/src/main/java/song/api/studio/controller/ConnectionController.java
@@ -1,12 +1,12 @@
 package song.api.studio.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import song.api.studio.service.IConnectionService;
-import song.common.result.ActionResult;
-import song.common.toolkit.base.BaseController;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+        import song.api.studio.service.IConnectionService;
+        import song.common.result.ActionResult;
+        import song.common.toolkit.base.BaseController;
 
 @RestController
 @RequestMapping("/conn")
