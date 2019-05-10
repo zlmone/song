ee6ca0815d92f6d03233ea6d43178c1936d5c1f4
songhuaxiaobao <1003859826@qq.com>
2019-05-10 19:53:56 +0800

    add

---
+++ java/song-api/song-api-studio/src/main/java/song/api/studio/controller/ConnectionController.java
@@ -0,0 +1,38 @@
+package song.api.studio.controller;
+
+ 
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+        import song.api.studio.service.IConnectionService;
+        import song.common.result.ActionResult;
+        import song.common.toolkit.base.BaseController;
+ 
+
+@RestController
+@RequestMapping("/conn")
+public class ConnectionController extends BaseController {
+    @Autowired
+    private IConnectionService connectionService;
+
+    @GetMapping(value = "/list")
+    public ActionResult getList() {
+        return getActionResult(connectionService.list());
+    }
+
+    @GetMapping(value = "/info")
+    public ActionResult getInfo(String id) {
+        return getActionResult(connectionService.getById(id));
+    }
+
+    @PostMapping(value = "/save")
+    public ActionResult save(@RequestBody Connection entity) {
+        return getSaveResult(connectionService.saveOrUpdate(entity));
+    }
+
+    @DeleteMapping(value = "/remove")
+    public ActionResult remove(String id) {
+        return getActionResult(connectionService.removeById(id));
+    }
+}
